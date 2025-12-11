e9dba8c365a9e8fa30570b4b17c124b555f0f1b6
Arthur <45236453+ArthurMJ96@users.noreply.github.com>
2025-12-11 09:39:48 +0100

    Initialize Spring Boot project

---
+++ src/main/java/lu/arthurmj/cnfpc_spring_boot_project_assignment/CnfpcSpringBootProjectAssignmentApplication.java
@@ -0,0 +1,13 @@
+package lu.arthurmj.cnfpc_spring_boot_project_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnfpcSpringBootProjectAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnfpcSpringBootProjectAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lu/arthurmj/cnfpc_spring_boot_project_assignment/CnfpcSpringBootProjectAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package lu.arthurmj.cnfpc_spring_boot_project_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnfpcSpringBootProjectAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
