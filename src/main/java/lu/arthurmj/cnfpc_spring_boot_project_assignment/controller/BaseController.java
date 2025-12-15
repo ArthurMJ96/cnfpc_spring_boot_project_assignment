08d00e8cc2d1ef0c4964d8de2527ce09f59adf7f
Arthur <45236453+ArthurMJ96@users.noreply.github.com>
2025-12-15 08:43:53 +0100

    Add WebConfig for locale resolution, implement product grid template, and enhance home page layout

--- src/main/java/lu/arthurmj/cnfpc_spring_boot_project_assignment/controller/BaseController.java
+++ src/main/java/lu/arthurmj/cnfpc_spring_boot_project_assignment/controller/BaseController.java
@@ -1,14 +1,21 @@
 package lu.arthurmj.cnfpc_spring_boot_project_assignment.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import lu.arthurmj.cnfpc_spring_boot_project_assignment.service.ProductService;
+
 @Controller
 public class BaseController {
 
+    @Autowired
+    private ProductService productService;
+
     @GetMapping("/")
     public String getBase(Model model) {
+        model.addAttribute("products", productService.findTop8Products());
         return "pages/home";
     }
 
