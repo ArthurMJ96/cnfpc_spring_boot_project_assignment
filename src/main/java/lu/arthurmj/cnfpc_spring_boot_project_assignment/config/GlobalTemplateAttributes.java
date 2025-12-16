6bca3b380a4ae69fab9f4bae6eb143c4034e0013
Arthur <45236453+ArthurMJ96@users.noreply.github.com>
2025-12-16 12:04:08 +0100

    Enhance Navigation UI with active path highlighting, dynamic titles, and improved navigation links

--- src/main/java/lu/arthurmj/cnfpc_spring_boot_project_assignment/config/GlobalTemplateAttributes.java
+++ src/main/java/lu/arthurmj/cnfpc_spring_boot_project_assignment/config/GlobalTemplateAttributes.java
@@ -1,21 +1,33 @@
 package lu.arthurmj.cnfpc_spring_boot_project_assignment.config;
 
+import java.util.function.Function;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class GlobalTemplateAttributes {
     @Value("${app.name}")
     private String appTitle;
 
+    /** Exposes the application title to Thymeleaf templates. */
     @ModelAttribute("appTitle")
     public String addGlobalAttributes() {
         return appTitle;
     }
 
-    // @ModelAttribute("Roles")
-    // public Class<Role> getRolesClass() {
-    // return Role.class;
-    // }
+    /**
+     * Exposes a Function to Thymeleaf to check for active paths.
+     * Returns "active" if the current path contains with the given path, else "".
+     * 
+     * @example `<div th:class="${uc.apply('/products')}">...</div>`
+     */
+    @ModelAttribute("uc")
+    public Function<String, String> urlContainsActive(HttpServletRequest request) {
+        String current = request.getRequestURI();
+        return path -> current != null && current.contains(path) ? "active" : "";
+    }
 }
